c4d3ae15dde762b7be74979907170d9aca387f64
Xenon <gonda.armand@gmail.com>
2021-03-05 09:36:56 +0100

    Updated file path, which was causing Heroku to throw 404 error.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const io = require('socket.io')(http, {
 	}});
 
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../socket-io-client/build')));
+app.use(express.static(path.resolve(__dirname, './socket-io-client/build')));
 // Handle GET requests to /api route
 app.get("/api", (req, res) => {
 	// res.json({ message: "Hello from server!" });
@@ -17,7 +17,7 @@ app.get("/api", (req, res) => {
 });
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
-	res.sendFile(path.resolve(__dirname, '../socket-io-client/build', 'index.html'));
+	res.sendFile(path.resolve(__dirname, './socket-io-client/build', 'index.html'));
 });
 
 // Socket.io
